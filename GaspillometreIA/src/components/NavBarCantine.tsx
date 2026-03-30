01623496741158a79b5e67baf7e978142da446e7
LucasTLLL <lescoat.g@stjolorient.fr>
2026-03-30 09:48:37 +0200

    FIX: texte dans les fichier modifier par un éleve d'un autre groupe

--- GaspillometreIA/src/components/NavBarCantine.tsx
+++ GaspillometreIA/src/components/NavBarCantine.tsx
@@ -9,7 +9,7 @@ const NavBarCantine = () => {
                 <ul className="menu menu-horizontal bg-secondary">
                     <li className="w-auto md:w-100 "><a onClick={() => navigate('/tendance')} className=" flex items-center justify-center">Visualiser les tendances </a></li>
                     <li className="w-auto md:w-100 "><a onClick={() => navigate('/rapport')} className=" flex items-center justify-center">Générer les rapports</a></li>
-                    <li className="w-auto md:w-100 "><a onClick={() => navigate('/menu')} className=" flex items-center justify-center">Importer les Rapports</a></li>
+                    <li className="w-auto md:w-100 "><a onClick={() => navigate('/menu')} className=" flex items-center justify-center">Importer les menus</a></li>
                 </ul></div>
         </div>
     )
